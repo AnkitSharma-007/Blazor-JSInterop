4389e585fe41b4c4cbb7678e5da4640b5cdde869
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2020-02-08 22:00:20 +0530

    Updated to .NET Core 3.2 Preview-1

---
+++ BlazorJSDemo/BlazorJSDemo/Pages/JSDemo.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorJSDemo.Pages
+{
+    public class JSDemoModel : ComponentBase
+    {
+        protected static string Message { get; set; }
+
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        [JSInvokable]
+        public static void CSCallBackMethod()
+        {
+            Message = "C# Method invoked";
+        }
+
+        protected async Task CallCSMethod()
+        {
+            await JSRuntime.InvokeAsync<object>("CSMethod");
+        }
+
+        protected void CallJSMethod()
+        {
+            JSRuntime.InvokeAsync<bool>("JSMethod");
+        }
+    }
+}
